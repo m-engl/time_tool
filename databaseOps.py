4c63f35f525ae9b98b70237acb27edae9793722f
m-engl <m.englart@gmail.com>
2020-03-27 09:58:43 +0100

    up

--- databaseOps.py
+++ databaseOps.py
@@ -1,61 +1,74 @@
 import sqlite3
+# import os ; print(os.getcwd())
 
-class DB_Creator:
+class DB_orders_to_execute:
 
-    db = "timetoolDB.sqlite"
+    def __init__(self):
 
-    create_times = """
-                  CREATE TABLE times IF NOT EXISTS (
-                      TimeID integer PRIMARY KEY,
-                      Start integer NOT NULL,
-                      Stop integer NOT NULL,
-                      CategoryID integer NOT NULL,
-                      FOREIGN KEY (CategoryID) REFERENCES categories (CategoryID)
-                  );
-                  """
+        self.db = "timetool.db"
 
-    create_categories = """
-                  CREATE TABLE categories IF NOT EXISTS (
-                      CategoryID integer NOT NULL PRIMARY KEY,
-                      CategoryName varchar(40) NOT NULL
-                  );        
-                  """
+        self.create_times = """
+             CREATE TABLE IF NOT EXISTS times (
+                 TimeID integer PRIMARY KEY,
+                 Start integer NOT NULL,
+                 Stop integer NOT NULL,
+                 CategoryID integer NOT NULL,
+                 FOREIGN KEY (CategoryID) REFERENCES categories (CategoryID)
+             );
+                             """
 
-    conn = None
+        self.create_categories = """
+             CREATE TABLE IF NOT EXISTS categories (
+                 CategoryID integer NOT NULL PRIMARY KEY,
+                 CategoryName varchar(40) NOT NULL
+             );        
+                                """
 
-    def create_connection(self, database, conn):
-        conn = self.conn
+class DB_Operator:
+
+    def __init__(self):
+
+        self.Orders = DB_orders_to_execute()
+
+    def connect(self, database):
+
+        conn = None
         try:
-            self.conn = sqlite3.connect(database)
-            return conn
+            conn = sqlite3.connect(database)
         except sqlite3.Error as error:
-            print(error),
+            print(error)
         return conn
-
 
     def create_table(self, conn, orders):
 
-        try:
-            c = conn.cursor()
-            c.execute(orders)
-            conn.commit()
-            cursor.close()
-
-        except sqlite3.Error as error:
-            print("Error:", error)
-
-        finally:
-            if (sqliteConnection):
-                sqliteConnection.close()
-                print("Connection closed.")
+        c = conn.cursor()
+        c.execute(orders)
+        conn.commit()
+        c.close()
 
     def create_database(self):
-        conn = create_connection(self.db, self.conn)
-        if conn is not None:
-            create_table(self.conn, self.create_times)
-            create_table(self.conn, self.create_categories)
-        else:
-            print("Something went wrong, conn = None?")
 
-        creator = DB_Creator()
-        creator.create_database()
+        conn = self.connect(self.Orders.db)
+
+        if conn is not None:
+            self.create_table(conn, self.Orders.create_times)
+            self.create_table(conn, self.Orders.create_categories)
+
+        else:
+            print("Something went wrong, see over connection")
+
+        conn.close()
+
+    def select(self, items):
+        # item = ("tablename", 'ITEMNAME',)
+        conn = self.connect(self.Orders.db)
+        c = conn.cursor()
+        c.execute('SELECT * FROM ?', items)
+        results = c.fetchall()
+        conn.close()
+        return results
+
+
+
+Damian = DB_Operator()
+Damian.create_database()
